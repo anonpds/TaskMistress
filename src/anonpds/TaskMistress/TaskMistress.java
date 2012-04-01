7b7873761cf0fb5f1881441688c3a71bdf3b2679
anonpds <anonpds@gmail.com>
2012-04-01 15:59:25 +0300

    Bumped version to 0.1d.

--- src/anonpds/TaskMistress/TaskMistress.java
+++ src/anonpds/TaskMistress/TaskMistress.java
@@ -33,7 +33,7 @@ public class TaskMistress {
 	public static final String PROGRAM_NAME = "Task Mistress";
 
 	/** The current version of the program. */
-	public static final String PROGRAM_VERSION = "0.1c";
+	public static final String PROGRAM_VERSION = "0.1d";
 
 	/** The name of the variable that contains the default task tree path. */
 	public static final String CONFIG_DEFAULT = "defaultTree";
